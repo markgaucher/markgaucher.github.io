026e8e772e260859b319fb1101186c861b215eaa
markgaucher <hello@markgaucher.com>
2017-08-07 19:00:27 -0400

    Moved icons to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
+import Github from './icons/Github';
+import Twitter from './icons/Twitter';
+
 import './Footer.scss';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <p>
-        &copy; 2017 Mark Gaucher | Site built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+        &copy; 2017 Mark Gaucher | Site built with <a href="https://www.gatsbyjs.org">Gatsby.js</a>
       </p>
+      <nav className="footer-menu">
+        <ul className="footer-menu__list">
+          <li className="footer-menu__list-item">
+            <a href="http://www.github.com/markgaucher">
+              <Github />
+            </a>
+          </li>
+          <li className="footer-menu__list-item">
+            <a href="http://www.twitter.com/markgaucher">
+              <Twitter />
+            </a>
+          </li>
+        </ul>
+      </nav>
     </footer>
   );
 };
